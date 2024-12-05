f5d9829c6288c4efb8fbb9d52cac4d55aafaa4bf
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2024-12-05 20:25:43 +0330

    add comment for time_interval_calculator

--- utils/time_interval_calculator.py
+++ utils/time_interval_calculator.py
@@ -12,10 +12,15 @@ def time_interval_calculator(time):
             - start_timestamp (int): Unix timestamp for start date
             - end_timestamp (int): Unix timestamp for end date (current time)
     """
+    # Get current date and time
     end_date = datetime.now()
+    # Calculate start date by subtracting given number of days from end date
     start_date = end_date - timedelta(days=time)
 
+    # Convert start date to Unix timestamp
     start_timestamp = int(start_date.timestamp())
+    # Convert end date to Unix timestamp 
     end_timestamp = int(end_date.timestamp())
 
+    # Return tuple of start and end timestamps
     return (start_timestamp, end_timestamp)
