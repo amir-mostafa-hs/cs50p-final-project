cade12f640964395d3e1d8de5a40097fe13acbde
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2024-12-05 11:51:39 +0330

    add docstring for cli_tool

--- utils/cli_tool.py
+++ utils/cli_tool.py
@@ -1,6 +1,23 @@
 from argparse import ArgumentParser
 
 def cli_tool(*args, description=""):
+    """
+    Creates a command line interface tool using argparse.
+    
+    Args:
+        *args: Variable length list of dictionaries containing argument specifications.
+              Each dictionary should have 'name', 'type' and 'help' keys.
+        description (str): Description of the command line tool. Defaults to empty string.
+    
+    Returns:
+        Namespace: Parsed command line arguments.
+    
+    Example:
+        args = cli_tool(
+            {"name": "--input", "type": str, "help": "Input file path"},
+            {"name": "--output", "type": str, "help": "Output file path"},
+            description="A file processing tool")
+    """
     parser = ArgumentParser(description=description)
 
     for arg in args:
